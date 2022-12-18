29310450ef0ffd824f062b4122d76bf706a59a3d
aspectie <kovleshenko99@mail.ru>
2022-12-18 18:42:48 +0300

    fix cell styling

--- src/components/Inventory/Cell.vue
+++ src/components/Inventory/Cell.vue
@@ -49,16 +49,30 @@ const props = defineProps({
         position: absolute;
         bottom: -1px;
         right: -1px;
-        padding: 10px;        
+        padding: 8px;        
         border: 1px solid #4D4D4D;
         font-family: 'SF';
         font-style: normal;
         font-weight: 500;
-        font-size: 16px;
-        line-height: 16px;
+        font-size: 12px;
         display: flex;
         align-items: center;
         text-align: center;
+        border-top-left-radius: 10px;
+        color: #FFFFFF;
+        opacity: 0.4;
+    }
+    &:first-child {
+        border-top-left-radius: 11px;
+    }
+    &:nth-child(5) {
+        border-top-right-radius: 11px;
+    }
+    &:nth-child(21) {
+        border-bottom-left-radius: 11px;
+    }
+    &:nth-child(25) {
+        border-bottom-right-radius: 11px;
     }
 }
 
